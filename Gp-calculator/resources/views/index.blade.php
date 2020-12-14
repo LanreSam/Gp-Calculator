831ee6c4a210137c316565ea51ef2df8215c6e5a
LanreSam <ezechelanre@gmail.com>
2020-12-13 21:59:55 -0600

    still working on the view

--- Gp-calculator/resources/views/index.blade.php
+++ Gp-calculator/resources/views/index.blade.php
@@ -1,11 +1,27 @@
-{{-- @extends('layouts.master') --}}
-@section('title', 'Home')
-@section('content')
-    <div class="container bg-success p-5">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 style="margin-top: 100rem;">Home Page Content</h1>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    {{-- font awesome icon --}}
+    <script src="https://kit.fontawesome.com/48c2562a43.js" crossorigin="anonymous"></script>
+
+    <style>
+
+    </style>
+</head>
+<body>
+
+    <div class="container-fluid fixed-top">
+
     </div>
-@endsection
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</html>
+
