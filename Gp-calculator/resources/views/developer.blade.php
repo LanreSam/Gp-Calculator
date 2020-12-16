a0a9398bf9662bf6cbaf38b78d4839c20f114f69
LanreSam <ezechelanre@gmail.com>
2020-12-16 22:24:05 +0100

    view

--- Gp-calculator/resources/views/developer.blade.php
+++ Gp-calculator/resources/views/developer.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.master')
 @section('title', 'Developer')
 @section('content')
+    <style>
+        .img{
+            width: 60%;
+        }
+    </style>
     <section style="margin-top: 10rem">
-        <div class="container-fluid ">
+        <div class="container d-lg-flex d-block">
             <div class="row">
                 <div class="col-6 col-md-12">
-                    <img src="images/dev.png" alt="Developer"/>
+                    <img src="images/dev.png" alt="Developer" class="img"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 col-md-12">
+                    <h1>Developer</h1>
                 </div>
             </div>
         </div>
