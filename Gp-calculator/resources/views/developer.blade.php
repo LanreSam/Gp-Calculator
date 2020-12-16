8e1e8454322c2a887165b65964621752fec58ad1
LanreSam <ezechelanre@gmail.com>
2020-12-16 22:44:53 +0100

    view

--- Gp-calculator/resources/views/developer.blade.php
+++ Gp-calculator/resources/views/developer.blade.php
@@ -3,13 +3,18 @@
 @section('content')
     <style>
         .img{
-            width: 60%;
+            width: 100%;
+        }
+        @media screen and (min-width: 769px){
+            .img{
+                width: 70%;
+            }
         }
     </style>
     <section style="margin-top: 10rem">
         <div class="container d-lg-flex d-block">
             <div class="row">
-                <div class="col-6 col-md-12">
+                <div class="col-6 col-md-12 d-flex justify-content-center">
                     <img src="images/dev.png" alt="Developer" class="img"/>
                 </div>
             </div>
