62536a115eba773e03c6810a8061c12701809d82
LanreSam <ezechelanre@gmail.com>
2020-12-17 06:42:52 +0100

    view

--- Gp-calculator/resources/views/developer.blade.php
+++ Gp-calculator/resources/views/developer.blade.php
@@ -21,7 +21,7 @@
                     <div class="col-lg-6 bg-success text-center p-5">
                         <h1>Ezeche Lanre Samuel</h1>
                         <p>
-                            A current 300L student of the prestigious Olabisi Onabanjo University (O.O.U)
+                            A current 300L Computer Science student of the prestigious Olabisi Onabanjo University (O.O.U)
                             as at the time of development of this application.
                         </p>
                     </div>
