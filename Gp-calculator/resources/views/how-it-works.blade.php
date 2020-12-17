788bed4749b33e7877f27c240963686998d5d323
LanreSam <ezechelanre@gmail.com>
2020-12-17 16:03:25 +0100

    how it works view

--- Gp-calculator/resources/views/how-it-works.blade.php
+++ Gp-calculator/resources/views/how-it-works.blade.php
@@ -25,6 +25,36 @@
                 </p>
             </div>
         </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 d-lg-flex d-block">
+                    <div class="col-lg-6 bg-dark text-center text-light p-5">
+                        <div class="p-4">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>id</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($users as $user)
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 bg-white shadow d-flex justify-content-center p-5">
+                        <img src="images/dev.png" alt="Developer" class="img"/>
+                    </div>
+                </div>
+            </div>
     </section>
 
 @endsection
