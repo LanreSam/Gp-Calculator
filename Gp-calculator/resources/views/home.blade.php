f3630b2f1160129c529cf0385e87c1fe3207dd75
LanreSam <ezechelanre@gmail.com>
2020-12-19 12:54:38 +0100

    view

--- Gp-calculator/resources/views/home.blade.php
+++ Gp-calculator/resources/views/home.blade.php
@@ -5,34 +5,38 @@
         <div class="container-fluid">
             <div class="row">
                 <h1>Welcome SCI/17/18/0494</h1>
-                 <table class="table">
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>course_code</th>
-                            <th>score</th>
-                            <th>course_unit</th>
-                            <th>grade</th>
-                            <th>points</th>
-                            <th>quality_points</th>
-                            <th>semester</th>
-                            <th>level</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>ID</td>
-                            <td>course_code</td>
-                            <td>Score</td>
-                            <td>course_unit</td>
-                            <td>Grade</td>
-                            <td>Points</td>
-                            <td>Quality_points</td>
-                            <td>Semester</td>
-                            <td>Level</td>
-                        </tr>
-                    </tbody>
-                </table>
+            </div>
+            <div class="container bg-white p-5 mt-5">
+                <div class="row">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>course_code</th>
+                                <th>score</th>
+                                <th>course_unit</th>
+                                <th>grade</th>
+                                <th>points</th>
+                                <th>quality_points</th>
+                                <th>semester</th>
+                                <th>level</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ID</td>
+                                <td>course_code</td>
+                                <td>Score</td>
+                                <td>course_unit</td>
+                                <td>Grade</td>
+                                <td>Points</td>
+                                <td>Quality_points</td>
+                                <td>Semester</td>
+                                <td>Level</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </section>
